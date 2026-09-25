daef606c522addef51f64a1ba0be9522998a4f44
Mihir Pukale <mihirpukale07@gmail.com>
2026-09-25 21:30:36 +0530

    Connect frontend login with backend API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:5000/api/auth/login",
+        "http://127.0.0.1:5000/api/users/login",
         {
           method: "POST",
           headers: {
